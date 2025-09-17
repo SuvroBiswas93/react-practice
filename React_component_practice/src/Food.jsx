af80ae7226960280b0cb27170120758225f9c035
Suvro Biswas <shuvo.249963@gmail.com>
2025-09-17 18:30:32 +0600

    React practice

--- React_component_practice/src/Food.jsx
+++ React_component_practice/src/Food.jsx
@@ -1,12 +1,33 @@
 export default function Food(props){
+
+    const foodStyle = {
+        color : 'green',
+        border : '3px solid tomato',
+        padding : '10px',
+        // listStyle:'none'
+
+    }
+
+    const listStyle = {
+        listStyle :'none',
+        fontWeight :'bold',
+        margin : '5px 0'
+    }
     // console.log(props)
     props.name()
+     const biryiani ={
+        name : 'mutton-biriyani',
+        name1:'chiken-biriyani',
+        name2:'chicken-biriyani'
+     }
     return (
         <>
-            <h1>Food That i love</h1>
-        <ul style={{ listStyleType: "none" }}>
-            <li style={{fontWeight : 'bold'}}>biriyani</li>
-            <li>Fish</li>
+            <h1 style={foodStyle}>Food That i love</h1>
+        <ul style={foodStyle}>
+            <li style={listStyle}>biriyani:</li>
+            <li style={listStyle}>{biryiani.name}</li>       
+            <li style={listStyle}>{biryiani.name1}</li>       
+            <li style={listStyle}>Fish</li>
         </ul>
         </>
     )
