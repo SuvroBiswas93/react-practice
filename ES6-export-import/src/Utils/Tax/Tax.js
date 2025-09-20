7c540833f8ba282bbaacd47a012b05e97be793a3
Suvro Biswas <shuvo.249963@gmail.com>
2025-09-21 00:17:24 +0600

    Practice export import of es6 inside react

---
+++ ES6-export-import/src/Utils/Tax/Tax.js
@@ -0,0 +1,7 @@
+import { mult } from "../Math/Math";
+
+//  export default const calculateTax = (mult(7,5)*.5)
+// console.log('Calculatex tax is',calculateTax)
+
+ export const calculateTax = (mult(7,5)*.5)
+
