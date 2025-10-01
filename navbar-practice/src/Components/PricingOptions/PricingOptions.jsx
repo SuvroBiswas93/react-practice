955c11f762c6089fb1d0fa156a269d42c2a945d2
Suvro Biswas <shuvo.249963@gmail.com>
2025-10-01 23:33:01 +0600

    practice pricing card with daisy price card and conditional rendering for mostpopular card

--- navbar-practice/src/Components/PricingOptions/PricingOptions.jsx
+++ navbar-practice/src/Components/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import CardPrice from '../CardPrice/CardPrice';
+import DaisyPriceCard from '../DaisyPriceCard/DaisyPriceCard';
 
 
 const PricingOptions = ({fetchPromise}) => {
@@ -9,8 +10,11 @@ const PricingOptions = ({fetchPromise}) => {
         <div>
             <h2 className='my-6 text-center text-3xl font-extrabold'>Get Our Membership</h2>
             <div className='grid grid-cols-1 mx-2 md:grid-cols-3 gap-6 '>
-                {
+                {/* {
                     priceData.map((pricing,id) =><CardPrice key={id} pricing={pricing}></CardPrice>) 
+                } */}
+                {
+                    priceData.map((pricing,id) =><DaisyPriceCard key={id} pricing={pricing}></DaisyPriceCard>) 
                 }
             </div>
         </div>
