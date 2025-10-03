4078f642103fa5e56fe3bfecf47bb2a0ced23d0d
Suvro Biswas <shuvo.249963@gmail.com>
2025-10-03 06:44:57 +0600

    Install react router and and tailwind css and practice data mode router

--- react-router-practice/src/main.jsx
+++ react-router-practice/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <div className='text-2xl font-bold mt-40 text-blue-700 animate-ping'>Hello from React router</div>,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router} />,
   </StrictMode>,
 )
