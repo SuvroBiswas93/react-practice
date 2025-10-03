8347fdea2910b994dd693d1fa209eb917f97e1c8
Suvro Biswas <shuvo.249963@gmail.com>
2025-10-03 08:49:20 +0600

    Practice basic of route

--- react-router-practice/src/main.jsx
+++ react-router-practice/src/main.jsx
@@ -8,8 +8,33 @@ import { RouterProvider } from "react-router/dom";
 const router = createBrowserRouter([
   {
     path:"/",
-    element: <div className='text-2xl font-bold mt-40 text-blue-700 animate-ping'>Hello from React router</div>,
+    element: <div className='text-2xl font-bold mt-40 text-blue-700 animate-bounce'>Hello from React router</div>,
   },
+  {
+    path:"app",
+    Component: App
+  },
+  {
+    path:'/about',
+    element : <div className='bg-yellow-900 p-5 text-2xl'>
+      This is about page
+      <div className='mt-20 bg-green-900 p-20 h-20 rounded-md'>
+          
+      </div>
+    </div>
+  },
+  {
+    path:'contact',
+    element: <div className='bg-purple-500 p-4 text-4xl rounded-2xl font-extrabold'>This is contact Page
+      <div className='mt-10 bg-green-300 p-20 rounded-md text-2xl font-extrabold text-center m-6'>
+        <div className='bg-yellow-100 p-10 flex justify-center items-center rounded-xl '>
+            Suvro Biswas<br />
+            Dhaka, Bangladesh 
+          </div> 
+      </div>
+    </div>
+  }
+
 ])
 
 createRoot(document.getElementById('root')).render(
